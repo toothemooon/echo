b114dd4cf23dd8c0b2631ea73cb16ec66406845d
サラダ <35186738+toothemooon@users.noreply.github.com>
2026-07-14 21:53:03 +0900

    ——

--- echo/src/app/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import "@/global.css";
-import { Stack } from "expo-router";
-import * as DevMenu from "expo-dev-client";
-import { useEffect } from "react";
-
-export default function RootLayout() {
-  return <Stack />;
-}
--- echo/src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View, Image, ScrollView } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text>hello world</Text>
-    </View>
-  );
-}
